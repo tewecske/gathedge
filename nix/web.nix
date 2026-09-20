f42f09f552bf0d78d47abf438a97600512e078d7
tewe <leventewe@gmail.com>
2026-09-20 23:03:42 +0200

    new release

--- nix/web.nix
+++ nix/web.nix
@@ -27,7 +27,7 @@ buildNpmPackage {
   sourceRoot = "${src.name}/web";
 
   # nix run nixpkgs#prefetch-npm-deps -- web/package-lock.json
-  npmDepsHash = "sha256-0tL+vhuf60FD2rvP3UaByHTjHklHgiJ0NtDGNKldWpI=";
+  npmDepsHash = "sha256-E+E5XTxP7aXuG7sqB0kwx/idjmdsRb1HZlWhUGTrYvw=";
 
   nativeBuildInputs = [ fakeSbt ];
 
